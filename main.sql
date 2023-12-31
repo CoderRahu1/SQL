45cee9fcb2c2d666b249ee8599b33ac58057e519
Er.Rahul Pillai <94741905+CoderRahu1@users.noreply.github.com>
2023-12-31 13:42:07 +0530

    Add files via upload

---
+++ main.sql
@@ -0,0 +1,13 @@
+-- create a table
+CREATE TABLE students (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  gender TEXT NOT NULL
+);
+-- insert some values
+INSERT INTO students VALUES (1, 'Ryan', 'M');
+INSERT INTO students VALUES (2, 'Joanna', 'F');
+INSERT INTO students VALUES (3, 'RAHUL', 'M');
+INSERT INTO students VALUES (4, 'PRANAV', 'M');
+-- fetch all values
+SELECT * FROM students;
