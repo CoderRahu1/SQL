03ebc632828fb669d1ebfe0cbcbe4266b5a8bdb7
Er.Rahul Pillai <94741905+CoderRahu1@users.noreply.github.com>
2023-12-31 13:43:18 +0530

    Update and rename main.sql to PROG_1.1.sql

--- PROG_1.1.sql
+++ PROG_1.1.sql
@@ -1,4 +1,4 @@
--- create a table
+-- create a table for the students
 CREATE TABLE students (
   id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
@@ -9,5 +9,5 @@ INSERT INTO students VALUES (1, 'Ryan', 'M');
 INSERT INTO students VALUES (2, 'Joanna', 'F');
 INSERT INTO students VALUES (3, 'RAHUL', 'M');
 INSERT INTO students VALUES (4, 'PRANAV', 'M');
--- fetch all values
+-- fetch all values the values that is there.
 SELECT * FROM students;
